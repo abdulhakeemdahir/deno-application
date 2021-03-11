33908556c5246f8e67e570548722ffa75214a199
Daniel <danielsoledad@gmail.com>
2021-03-11 11:18:45 -0800

    ading mail class

--- config/options.js
+++ config/options.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 const options = {
   access: process.env.ACCESS,
-  secret: process.env.SECRET
+  secret: process.env.SECRET,
+  mailPass: process.env.MAIL,
+  siteUrl: process.env.SITEURL
 };
 
 module.exports = key => options[key];
