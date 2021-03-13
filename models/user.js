c3447424f0d603cc6e573ec6b568b1fdc87739e3
Taani Maama <taani1maama@live.com>
2021-03-13 11:33:16 -0800

    Updated user model

--- models/user.js
+++ models/user.js
@@ -91,13 +91,4 @@ const Organization = User.discriminator(
   option
 );
 
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, isMatch);
-  });
-};
-
 module.exports = { User, Organization };
