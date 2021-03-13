c82c7455526a8cf4d57a9df4154b4a339fc1cd54
Taani Maama <taani1maama@live.com>
2021-03-13 11:30:00 -0800

    Updated user model.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-const SALT_WORK_FACTOR = 10;
 const option = { discriminatorKey: "org" };
 
 const userSchema = new Schema({
@@ -34,18 +32,10 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
-  tokens: [
-    {
-      access: {
-        type: String,
-        required: true
-      },
-      token: {
-        type: String,
-        required: true
-      }
-    }
-  ],
+  uuid: {
+    type: String,
+    required: true
+  },
   role: {
     type: String,
     enum: ["Organization", "Personal"],
@@ -100,33 +90,6 @@ const Organization = User.discriminator(
   organizationSchema,
   option
 );
-
-userSchema.pre("save", function(next) {
-  const user = this;
-
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified("password")) {
-    return next();
-  }
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) {
-      return next(err);
-    }
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, (err, hash) => {
-      if (err) {
-        return next(err);
-      }
-
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
-    });
-  });
-});
 
 userSchema.methods.comparePassword = function(candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
