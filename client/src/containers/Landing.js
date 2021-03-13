e6275158128e91491a32a93c6cbb1fc23d74885e
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-13 11:46:51 -0800

    fixed footer problem

--- client/src/containers/Landing.js
+++ client/src/containers/Landing.js
@@ -1,7 +1,7 @@
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Gradient from "../components/Gradient";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 import Welcome from "../components/Welcome";
 import Signup from "../components/forms/Signup";
 import Signin from "../components/forms/Signin";
