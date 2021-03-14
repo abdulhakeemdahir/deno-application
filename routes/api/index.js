53bd6907e9ec06505cdf4a03ca8a92f71a1d0b47
Taani Maama <taani1maama@live.com>
2021-03-13 20:16:31 -0800

    Updated the API index to use the causes routes and controller.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const user = require("./user");
 const posts = require("./posts");
+const causes = require("./causes");
 
 //user  routes
 router.use("/user", user);
 router.use("/posts", posts);
+router.use("/causes", causes);
 
 module.exports = router;
