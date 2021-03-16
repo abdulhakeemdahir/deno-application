79db557c3086ed222cdd111009efe49642fa4a3d
Taani Maama <taani1maama@live.com>
2021-03-15 20:44:47 -0700

    Updated the index for messenger routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,13 @@ const user = require("./user");
 const posts = require("./posts");
 const causes = require("./causes");
 const hashtags = require("./hashtags");
+const messenger = require("./messenger");
 
 //user  routes
 router.use("/user", user);
 router.use("/posts", posts);
 router.use("/causes", causes);
 router.use("/hashtags", hashtags);
+router.use("/messenger", messenger);
 
 module.exports = router;
