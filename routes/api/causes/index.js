9c3b42a0732163cdf11bdbf6fd4d7baee0a5b83d
daniel <danielsoledad@gmail.com>
2021-03-15 21:44:24 -0700

    stashing changes before pulling

--- routes/api/causes/index.js
+++ routes/api/causes/index.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
-const { causeController } = require("../../../controllers");
+const { causesController } = require("../../../controllers");
 
 // The Causes that the user will get when they view their Causes tab.
 router
   .route("/dashboard")
-  .get(causeController.getUsersCauses)
-  .get(causeController.getTrending);
+  .get(causesController.getUsersCauses)
+  .get(causesController.getTrending);
 
 // The route for creating a cause.
-router.route("/create/cause").post(causeController.create);
+router.route("/create/cause").post(causesController.create);
 
 // The route for updating or deleting a cause.
 router
   .route("/update/cause/:id")
-  .put(causeController.update)
-  .delete(causeController.remove);
+  .put(causesController.update)
+  .delete(causesController.remove);
 
 module.exports = router;
