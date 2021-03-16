af55816f0b8e40116e9f635eaa6e2b7bf5e56f07
Taani Maama <taani1maama@live.com>
2021-03-15 20:15:18 -0700

    Updated the index to get the hashtag routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const user = require("./user");
 const posts = require("./posts");
 const causes = require("./causes");
+const hashtags = require("./hashtags");
 
 //user  routes
 router.use("/user", user);
 router.use("/posts", posts);
 router.use("/causes", causes);
+router.use("/hashtags", hashtags);
 
 module.exports = router;
