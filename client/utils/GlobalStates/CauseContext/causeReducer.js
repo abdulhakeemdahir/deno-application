031fdfdc260140ad1f9327479cae8c5644a6d641
Astral Gnome <reedkeenan@gmail.com>
2021-03-17 16:52:32 -0700

    pulling folder capitalization corrections

---
+++ client/utils/GlobalStates/CauseContext/causeReducer.js
@@ -0,0 +1,22 @@
+import {
+  GET_CAUSE_INFO,
+  ADD_CAUSE,
+  REMOVE_CAUSE,
+  UPDATE_CAUSE,
+  CAUSE_LOADING,
+  CAUSE_LOADED
+} from "../../actions/actions";
+
+export const reducer = (state, { type, payload }) => {
+  switch (type) {
+
+  case GET_CAUSE_INFO:
+    return {
+      ...state,
+      ...payload }
+
+  default:
+    console.log("LOREMIPSUMASKDFHAKLSDHFKASJDHF;LASJDHFLASJDFH");
+    return state
+  }
+}
