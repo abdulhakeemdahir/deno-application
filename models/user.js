8a7e6d77a1765f791aa2dc232083a0b0eae838d9
Taani Maama <taani1maama@live.com>
2021-03-18 13:01:52 -0700

    Updated user model for org info.

--- models/user.js
+++ models/user.js
@@ -5,72 +5,75 @@ const Schema = mongoose.Schema;
 
 const option = { discriminatorKey: "org" };
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  lastname: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-    //validate: [validateEmail, "Please fill a valid email address."],
-    match: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  uuid: {
-    type: String,
-    required: false
-  },
-  role: {
-    type: String,
-    enum: ["Organization", "Personal"],
-    required: [true, "Must choose a role."]
-  },
-  verified: {
-    type: Boolean
-  },
-  following: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Users"
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    lastname: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      //validate: [validateEmail, "Please fill a valid email address."],
+      match: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    uuid: {
+      type: String,
+      required: false
+    },
+    role: {
+      type: String,
+      enum: ["Organization", "Personal"],
+      required: [true, "Must choose a role."]
+    },
+    verified: {
+      type: Boolean
+    },
+    following: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users"
+      }
+    ],
+    followers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users"
+      }
+    ],
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Posts"
+      }
+    ],
+    profileImg: {
+      type: String
+    },
+    bannerImg: {
+      type: String
     }
-  ],
-  followers: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Users"
-    }
-  ],
-  posts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Posts"
-    }
-  ],
-  profileImg: {
-    type: String
   },
-  bannerImg: {
-    type: String
-  }
-});
+  { timestamps: true }
+);
 
 const organizationSchema = new Schema({
   causes: [
@@ -83,6 +86,29 @@ const organizationSchema = new Schema({
     type: String,
     required: false,
     trim: true
+  },
+  phoneNumber: {
+    type: String,
+    trim: true,
+    match: /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g
+  },
+  website: mongoose.SchemaTypes.Url,
+  address: {
+    street: {
+      type: String,
+      aptNo: {
+        type: String,
+        required: false
+      }
+    },
+    city: String,
+    state: String,
+    zipCode: Number,
+    country: String
+  },
+  authenticated: {
+    type: Boolean,
+    default: false
   }
 });
 
