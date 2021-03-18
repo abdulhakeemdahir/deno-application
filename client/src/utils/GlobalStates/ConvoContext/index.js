0c1b9ac55ef233ee8a0effe874250670182562eb
Taani Maama <taani1maama@live.com>
2021-03-17 20:56:24 -0700

    Created the ConvoContext

---
+++ client/src/utils/GlobalStates/ConvoContext/index.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useState } from "react";
+
+const ConvoContext = createContext();
+const { Provider } = ConvoContext;
+
+const ConvoProvider = ({ children }) => {
+  const [convos, setConvos] = useState({
+    converstaions: [{ messages: [] }]
+  });
+
+  const newConvo = (id, username) => {
+    setConvos(prevConvos => {
+      return [...prevConvos, { id, username }];
+    });
+  };
+
+  return <Provider value={[convos, newConvo]} />;
+};
+
+const useConvoContext = () => {
+  return useContext(ConvoContext);
+};
+
+export { ConvoProvider, useConvoContext };
