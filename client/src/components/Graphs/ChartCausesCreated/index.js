fe7dd33e0f894175670612ddc01cb191f81f95a5
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-20 21:30:49 -0700

    added analytics page

---
+++ client/src/components/Graphs/ChartCausesCreated/index.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { HorizontalBar } from "react-chartjs-2";
+
+import { Typography, Grid, Divider, CardContent } from "@material-ui/core";
+
+import "./style.css";
+
+export default function ChartCausesCreated(props) {
+	return (
+		<>
+			<Grid item className='card' xs={12}>
+				<Grid container className='headerContainer'>
+					<Grid item xs={9} sm={10}>
+						<Typography variant='subtitle1' style={{ fontWeight: "bold" }}>
+							{props.title}
+						</Typography>
+					</Grid>
+				</Grid>
+				<Typography variant='body2' color='textSecondary' component='p'>
+					<span className='authorStyle'> Causes Created:</span>
+				</Typography>
+				<Divider />
+				<Grid container direction='row' spacing={1}>
+					<CardContent>
+						<HorizontalBar
+							data={{
+								labels: [
+									"January",
+									"February",
+									"March",
+									"April",
+									"May",
+									"June",
+									"July",
+									"August",
+									"September",
+									"October",
+									"November",
+									"December",
+								],
+								datasets: [
+									{
+										label: "Causes Over Time",
+										data: [1, 0, 3, 1, 0, 0, 2, 1, 1, 3, 0, 0],
+										backgroundColor: [
+											"rgba(255, 99, 132, 0.2)",
+											"rgba(54, 162, 235, 0.2)",
+											"rgba(255, 206, 86, 0.2)",
+											"rgba(75, 192, 192, 0.2)",
+											"rgba(153, 102, 255, 0.2)",
+											"rgba(255, 159, 64, 0.2)",
+											"rgba(255, 99, 132, 0.2)",
+											"rgba(54, 162, 235, 0.2)",
+											"rgba(255, 206, 86, 0.2)",
+											"rgba(75, 192, 192, 0.2)",
+											"rgba(153, 102, 255, 0.2)",
+											"rgba(255, 159, 64, 0.2)",
+										],
+										borderColor: [
+											"rgba(255, 99, 132, 1)",
+											"rgba(54, 162, 235, 1)",
+											"rgba(255, 206, 86, 1)",
+											"rgba(75, 192, 192, 1)",
+											"rgba(153, 102, 255, 1)",
+											"rgba(255, 159, 64, 1)",
+											"rgba(255, 99, 132, 1)",
+											"rgba(54, 162, 235, 1)",
+											"rgba(255, 206, 86, 1)",
+											"rgba(75, 192, 192, 1)",
+											"rgba(153, 102, 255, 1)",
+											"rgba(255, 159, 64, 1)",
+										],
+										borderWidth: 1,
+									},
+								],
+							}}
+							height={400}
+							width={200}
+							options={{
+								maintainAspectRatio: false,
+							}}
+						/>
+					</CardContent>
+				</Grid>
+			</Grid>
+		</>
+	);
+}
