64ec75ee310592b918b7e9efb7797843b9aea6b7
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-22 14:22:56 -0700

    added 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -15,69 +15,71 @@ import { useStoreContext } from "./utils/GlobalStates/AuthStore";
 import Chatroom from "./containers/Private/Chatroom";
 import SinglePost from "./containers/SinglePost";
 import Analytics from "./containers/Private/Analytics";
+import ErrorPage from "./containers/Public/ErrorPage";
 //import GuestRoute from "./components/GuestRoute.js"
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#1dc4e9"
-    },
-    secondary: { main: "#d500f9" },
-    default: {
-      main: "#454545"
-      // second: "#899fd4"
-    }
-  }
+	palette: {
+		primary: {
+			main: "#1dc4e9",
+		},
+		secondary: { main: "#d500f9" },
+		default: {
+			main: "#454545",
+			// second: "#899fd4"
+		},
+	},
 });
 function App() {
-  const [state] = useStoreContext();
-  useSocketConnection();
+	const [state] = useStoreContext();
+	useSocketConnection();
 
-  console.log(state);
+	console.log(state);
 
-  useAuthTokenStore();
+	useAuthTokenStore();
 
-  return (
-    <Router>
-      <MuiThemeProvider theme={theme}>
-        <div className='App'>
-          <UserProvider>
-            <CauseProvider>
-              <PostProvider>
-                <Switch>
-                  <PrivateRoute
-                    exact
-                    path='/newsfeed'
-                    redirectTo='/'
-                    component={Newsfeed}
-                  />
+	return (
+		<Router>
+			<MuiThemeProvider theme={theme}>
+				<div className='App'>
+					<UserProvider>
+						<CauseProvider>
+							<PostProvider>
+								<Switch>
+									<PrivateRoute
+										exact
+										path='/newsfeed'
+										redirectTo='/'
+										component={Newsfeed}
+									/>
 
-                  <PrivateRoute
-                    exact
-                    path='/dashboard'
-                    redirectTo='/'
-                    component={Dashboard}
-                  />
-                  <PrivateRoute
-                    exact
-                    path='/chatroom'
-                    redirectTo='/'
-                    component={Chatroom}
-                  />
+									<PrivateRoute
+										exact
+										path='/dashboard'
+										redirectTo='/'
+										component={Dashboard}
+									/>
+									<PrivateRoute
+										exact
+										path='/chatroom'
+										redirectTo='/'
+										component={Chatroom}
+									/>
 
-                  <Route path='/explore' exact component={Explore} />
-                  <Route path='/post' exact component={SinglePost} />
-                  <Route path='/analytics' exact component={Analytics} />
+									<Route path='/explore' exact component={Explore} />
+									<Route path='/post' exact component={SinglePost} />
+									<Route path='/analytics' exact component={Analytics} />
+									<Route path='/404' exact component={ErrorPage} />
 
-                  <Route path='/' exact component={Landing} />
-                </Switch>
-              </PostProvider>
-            </CauseProvider>
-          </UserProvider>
-        </div>
-      </MuiThemeProvider>
-    </Router>
-  );
+									<Route path='/' exact component={Landing} />
+								</Switch>
+							</PostProvider>
+						</CauseProvider>
+					</UserProvider>
+				</div>
+			</MuiThemeProvider>
+		</Router>
+	);
 }
 
 export default App;
