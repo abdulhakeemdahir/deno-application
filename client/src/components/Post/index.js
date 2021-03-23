2b7789ae51493a76a12b03f8904e6f51346fbb02
Abdulhakeem Dahir <somlutions@gmail.com>
2021-03-23 14:42:59 -0700

    merge fix

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -10,10 +10,15 @@ import "./style.css";
 import { useState } from "react";
 import { useUserContext } from "../../utils/GlobalStates/UserContext";
 import API from "../../utils/api.js";
-import { ADD_CAUSE, ADD_POST, CAUSE_LOADING, POST_LOADING } from "../../utils/actions/actions";
+import {
+	ADD_CAUSE,
+	ADD_POST,
+	CAUSE_LOADING,
+	POST_LOADING,
+} from "../../utils/actions/actions";
 import { usePostContext } from "../../utils/GlobalStates/PostContext";
 import { useCauseContext } from "../../utils/GlobalStates/CauseContext";
-import findHashtags from "find-hashtags"
+import findHashtags from "find-hashtags";
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -43,185 +48,186 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function Post() {
-  const [, causeDispatch] = useCauseContext();
-  const [, postDispatch] = usePostContext();
-  const classes = useStyles();
+	const [, causeDispatch] = useCauseContext();
+	const [, postDispatch] = usePostContext();
+	const classes = useStyles();
 
-  //*Create Post
-  const addPost = async (postInfo) => {
-    postDispatch({ type: POST_LOADING });
-    postDispatch({
-      type: ADD_POST,
-      payload: {
-        ...postInfo,
-        loading: false,
-      },
-    });
-  };
+	//*Create Post
+	const addPost = async postInfo => {
+		postDispatch({ type: POST_LOADING });
+		postDispatch({
+			type: ADD_POST,
+			payload: {
+				...postInfo,
+				loading: false,
+			},
+		});
+	};
 
-//Create cause
-const addCause = async (causeInfo) => {
-  causeDispatch({ type: CAUSE_LOADING});
-  causeDispatch({
-    type: ADD_CAUSE,
-    payload: {
-      ...causeInfo,
-      loading: false,
-    },
-  });
-};
-
+	//Create cause
+	const addCause = async causeInfo => {
+		causeDispatch({ type: CAUSE_LOADING });
+		causeDispatch({
+			type: ADD_CAUSE,
+			payload: {
+				...causeInfo,
+				loading: false,
+			},
+		});
+	};
 
 	const [createPost, setCreatePost] = useState({
-		type:"",
+		type: "",
 		title: "",
 		content: "",
-		imageUrl:""
-  });
+		imageUrl: "",
+	});
 
-  const handleChange = function(event) {
-    const { name, value } = event.target;
-    setCreatePost({
-      ...createPost,
-      [name]: value,
-    });
-  };
+	const handleChange = function(event) {
+		const { name, value } = event.target;
+		setCreatePost({
+			...createPost,
+			[name]: value,
+		});
+	};
 
-  const [userState, ] = useUserContext();
+	const [userState] = useUserContext();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    if(createPost.type === "" || createPost.title === "" || createPost.content === "" || createPost.imageUrl === ""){
-      return
-    }
+	const handleSubmit = async event => {
+		event.preventDefault();
+		if (
+			createPost.type === "" ||
+			createPost.title === "" ||
+			createPost.content === "" ||
+			createPost.imageUrl === ""
+		) {
+			return;
+		}
 		try {
-      
 			const post = {
 				...createPost,
 				author: userState._id,
+			};
+
+			const hashtags = await findHashtags(createPost.content);
+
+			if (hashtags.length) {
+				const createHashtags = await API.createHashtag({ hashtag: hashtags });
+				post.hashtags = createHashtags.data._id;
+			}
+
+			if (createPost.type === "Post") {
+				const { data } = await API.createPost(post);
+				if (post.hashtags) {
+					await API.updateHashtag(post.hashtags, {
+						posts: data._id,
+					});
+				}
+
+				await API.updateUser(post.author, {
+					posts: data._id,
+				});
+
+				addPost(data);
+				return;
+			} else {
+				const { data } = await API.createCause(post);
+
+				if (post.hashtags) {
+					await API.updateHashtag(post.hashtags, {
+						causes: data._id,
+					});
+				}
+
+				await API.updateUser(post.author, {
+					causes: data._id,
+				});
+
+				addCause(data);
+			}
+
+			clearState();
+		} catch (err) {
+			console.log("here", err);
 		}
-
-    const hashtags = await findHashtags(createPost.content);
-
-    if (hashtags.length) {
-      const createHashtags = await API.createHashtag({ hashtag:hashtags });
-      post.hashtags = createHashtags.data._id
-    }
-
-		if(createPost.type === "Post"){
-			
-      const {data} = await API.createPost(post);
-      if (post.hashtags){
-          await API.updateHashtag(post.hashtags, {
-            posts: data._id,
-          });
-      }
-      
-      await API.updateUser(post.author, {
-        posts: data._id,
-      });
-
-      addPost(data);
-      return
-    }else{
-        const { data } = await API.createCause(post);
-
-        if (post.hashtags) {
-          await API.updateHashtag(post.hashtags, {
-            causes: data._id,
-          });
-        }
-
-        await API.updateUser(post.author, {
-          causes: data._id,
-        });
-
-        addCause(data);
-      }
-		
-    clearState();
-
-}catch (err) {
-      console.log("here", err)
-    }
-}
-const clearState = () =>{
-    setCreatePost({
-      type: "",
-      title: "",
-      content: "",
-      imageUrl: "",
-    });
-    return
-}
+	};
+	const clearState = () => {
+		setCreatePost({
+			type: "",
+			title: "",
+			content: "",
+			imageUrl: "",
+		});
+		return;
+	};
 
 	return (
-    <Grid className="cardPost">
-      <form
-        className={classes.root}
-        noValidate
-        autoComplete="off"
-        onSubmit={handleSubmit}
-      >
-        <FormControl variant="outlined">
-          <InputLabel id="post">Post Type</InputLabel>
-          <Select
-            labelId="post"
-            id="post"
-            label="post type"
-            name="type"
-            onChange={handleChange}
-          >
-            <MenuItem value={"Post"}>Post</MenuItem>
-            <MenuItem value={"Cause"}>Cause</MenuItem>
-          </Select>
-        </FormControl>
-        <div>
-          <Grid container>
-            <TextField
-              name="title"
-              value={createPost.title}
-              onChange={handleChange}
-              id="title"
-              label="Title"
-              multiline
-              rowsMax={4}
-              className={classes.inputMargin}
-              size="small"
-            />
-            <TextField
-              name="imageUrl"
-              value={createPost.imageUrl}
-              onChange={handleChange}
-              id="imageUrl"
-              label="Image Url"
-              multiline
-              rowsMax={4}
-              className={classes.inputMargin}
-              size="small"
-            />
-            <TextField
-              name="content"
-              value={createPost.content}
-              onChange={handleChange}
-              id="post"
-              label="Post a Message"
-              variant="filled"
-              multiline
-              rows={4}
-              fullWidth
-              size="small"
-            />
-          </Grid>
-        </div>
-        <Button
-          size="small"
-          className={classes.styleMain}
-          onClick={handleSubmit}
-        >
-          <ChatBubbleOutlineIcon /> Post
-        </Button>
-      </form>
-    </Grid>
-  );
+		<Grid className='cardPost'>
+			<form
+				className={classes.root}
+				noValidate
+				autoComplete='off'
+				onSubmit={handleSubmit}
+			>
+				<FormControl variant='outlined'>
+					<InputLabel id='post'>Post Type</InputLabel>
+					<Select
+						labelId='post'
+						id='post'
+						label='post type'
+						name='type'
+						onChange={handleChange}
+					>
+						<MenuItem value={"Post"}>Post</MenuItem>
+						<MenuItem value={"Cause"}>Cause</MenuItem>
+					</Select>
+				</FormControl>
+				<div>
+					<Grid container>
+						<TextField
+							name='title'
+							value={createPost.title}
+							onChange={handleChange}
+							id='title'
+							label='Title'
+							multiline
+							rowsMax={4}
+							className={classes.inputMargin}
+							size='small'
+						/>
+						<TextField
+							name='imageUrl'
+							value={createPost.imageUrl}
+							onChange={handleChange}
+							id='imageUrl'
+							label='Image Url'
+							multiline
+							rowsMax={4}
+							className={classes.inputMargin}
+							size='small'
+						/>
+						<TextField
+							name='content'
+							value={createPost.content}
+							onChange={handleChange}
+							id='post'
+							label='Post a Message'
+							variant='filled'
+							multiline
+							rows={4}
+							fullWidth
+							size='small'
+						/>
+					</Grid>
+				</div>
+				<Button
+					size='small'
+					className={classes.styleMain}
+					onClick={handleSubmit}
+				>
+					<ChatBubbleOutlineIcon /> Post
+				</Button>
+			</form>
+		</Grid>
+	);
 }
--- client/src/components/Private/NewsAndComment/index.js
+++ client/src/components/Private/NewsAndComment/index.js
@@ -15,13 +15,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import "./style.css";
-<<<<<<< HEAD
 import { Favorite } from "@material-ui/icons";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-=======
 import { useUserContext } from "../../../utils/GlobalStates/UserContext";
 import api from "../../../utils/api";
->>>>>>> 38024bc85f2174b6af066984ea45eea2cf34f6dd
 const useStyles = makeStyles(theme => ({
 	root: {
 		width: "100%",
@@ -61,8 +58,45 @@ const useStyles = makeStyles(theme => ({
 	},
 }));
 export default function NewsAndComment(props) {
-<<<<<<< HEAD
 	const classes = useStyles();
+
+	const [userState] = useUserContext();
+
+	const [open, setOpen] = useState(false);
+
+	const [commentState, setCommentState] = useState({
+		content: "",
+	});
+
+	const handleChange = function(event) {
+		const { name, value } = event.target;
+		setCommentState({
+			...commentState,
+			[name]: value,
+		});
+	};
+
+	const handleSubmit = async id => {
+		try {
+			const comment = {
+				...commentState,
+				user: userState._id,
+				post: id,
+			};
+
+			const { data } = await api.createComments(comment);
+
+			await api.updatePost(id, { comments: data._id });
+		} catch (err) {}
+	};
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+	const handleClose = () => {
+		setOpen(false);
+	};
+
 	const [like, setLike] = React.useState(false);
 	const handleLike = () => {
 		setLike(!like);
@@ -106,7 +140,10 @@ export default function NewsAndComment(props) {
 				<Grid container xs={12} spacing={1}>
 					<Grid item xs={12} sm={8}>
 						<TextField
-							id='post'
+							name='content'
+							value={commentState.content}
+							onChange={handleChange}
+							id={props.id}
 							label='Post a Comment'
 							variant='filled'
 							size='small'
@@ -115,9 +152,15 @@ export default function NewsAndComment(props) {
 							fullWidth
 						/>
 					</Grid>
-					<Grid item xs={12} sm={4}>
-						<Button size='small' className={classes.styleMain} fullWidth>
-							<ChatBubbleOutlineIcon /> Comment
+					<Grid item xs={12} sm={4} id={props.id}>
+						<Button
+							size='small'
+							id={props.id}
+							className={classes.styleMain}
+							fullWidth
+							onClick={() => handleSubmit(props.id)}
+						>
+							<ChatBubbleOutlineIcon id={props.id} /> Comment
 						</Button>
 					</Grid>
 					{props.comments.length <= 0 ? (
@@ -141,7 +184,7 @@ export default function NewsAndComment(props) {
 													color='textSecondary'
 													component='p'
 												>
-													{card.author}
+													{card.user.firstName}
 												</Typography>
 											</Grid>
 											<Grid item xs={8}>
@@ -150,7 +193,7 @@ export default function NewsAndComment(props) {
 													color='textSecondary'
 													component='p'
 												>
-													{card.post}
+													{card.content}
 												</Typography>
 											</Grid>
 										</Grid>
@@ -163,150 +206,4 @@ export default function NewsAndComment(props) {
 			</Grid>
 		</>
 	);
-=======
-    const classes = useStyles();
-
-    const [userState] = useUserContext();
-
-    const [open, setOpen] = useState(false);
-
-    const [commentState, setCommentState] = useState({
-      content: "",
-    });
-
-    const handleChange = function(event) {
-      const { name, value } = event.target;
-      setCommentState({
-        ...commentState,
-        [name]: value,
-      });
-    };
-
-    const handleSubmit = async id => {
-		
-    try{
-        const comment = {
-          ...commentState,
-          user: userState._id,
-          post: id,
-        };
-
-        const { data } = await api.createComments(comment);
-
-        await api.updatePost(id, {comments:data._id});
-
-    }catch(err){
-
-    }
-  
-  }
-
-    const handleOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-    return (
-      <>
-        <Grid item className="card" xs={12}>
-          <Grid container className="headerContainer">
-            <Grid item xs={9} sm={10}>
-              <Typography variant="subtitle1" style={{ fontWeight: "bold" }}>
-                {props.title}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Typography variant="body2" color="textSecondary" component="p">
-            <span className="authorStyle"> Author:</span> {props.author}
-          </Typography>
-          <Divider />
-          <Grid container direction="row" spacing={1}>
-            <Grid item xs={12} sm={4}>
-              <CardMedia className={"media"} image={props.image} />
-            </Grid>
-            <Grid item xs={12} sm={8}>
-              <CardContent>
-                <Typography variant="body" color="textSecondary" component="p">
-                  {props.post}
-                </Typography>
-                <a href={props.link} className="hashTagStyle">
-                  #{props.hashTag}
-                </a>
-              </CardContent>
-              <Divider />
-            </Grid>
-          </Grid>
-          <Grid container xs={12} spacing={1}>
-            <Grid item xs={12} sm={8}>
-              <TextField
-                name="content"
-                value={commentState.content}
-                onChange={handleChange}
-                id={props.id}
-                label="Post a Comment"
-                variant="filled"
-                size="small"
-                multiline
-                rowsMax={4}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={12} sm={4} id={props.id}>
-              <Button
-                size="small"
-                id={props.id}
-                className={classes.styleMain}
-                fullWidth
-                onClick={() => handleSubmit(props.id)}
-              >
-                <ChatBubbleOutlineIcon id={props.id} /> Comment
-              </Button>
-            </Grid>
-            {props.comments.length >= 0 ? (
-              <Accordion className={classes.shadow}>
-                <AccordionSummary
-                  expandIcon={
-                    <ExpandMoreIcon className={classes.commentStyle} />
-                  }
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                >
-                  <Typography className={classes.heading}>
-                    Read {props.comments.length} Comments
-                  </Typography>
-                </AccordionSummary>
-                <Grid className="cardComment">
-                  {props.comments.map((card) => (
-                    <AccordionDetails>
-                      <Grid container xs={12} className={classes.gridStyle}>
-                        <Grid item xs={4}>
-                          <Typography
-                            variant="body"
-                            color="textSecondary"
-                            component="p"
-                          >
-                            {card.user.firstName}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={8}>
-                          <Typography
-                            variant="body"
-                            color="textSecondary"
-                            component="p"
-                          >
-                            {card.content}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </AccordionDetails>
-                  ))}
-                </Grid>
-              </Accordion>
-            ) : null}
-          </Grid>
-        </Grid>
-      </>
-    );
->>>>>>> 38024bc85f2174b6af066984ea45eea2cf34f6dd
 }
