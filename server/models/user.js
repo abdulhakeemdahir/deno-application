e20c944f62240527b07d800627b78e15597715ff
Taani Maama <taani1maama@live.com>
2021-03-24 18:36:12 -0700

    Updated the user model.

--- server/models/user.js
+++ server/models/user.js
@@ -5,77 +5,6 @@ const Schema = mongoose.Schema;
 
 const option = { discriminatorKey: "org" };
 
-<<<<<<< HEAD:models/user.js
-const userSchema = new Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    lastname: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    username: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true,
-      //validate: [validateEmail, "Please fill a valid email address."],
-      match: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    uuid: {
-      type: String,
-      required: false
-    },
-    role: {
-      type: String,
-      enum: ["Organization", "Personal"],
-      required: [true, "Must choose a role."]
-    },
-    verified: {
-      type: Boolean
-    },
-    following: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Users"
-      }
-    ],
-    followers: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Users"
-      }
-    ],
-    posts: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Posts"
-      }
-    ],
-    profileImg: {
-      type: String
-    },
-    bannerImg: {
-      type: String
-    }
-  },
-  { timestamps: true }
-);
-=======
 const userSchema = new Schema({
   firstName: {
     type: String,
@@ -155,7 +84,6 @@ const userSchema = new Schema({
     type: String
   }
 });
->>>>>>> 955e439aafce4876adf210344085ba80a671ac40:server/models/user.js
 
 const organizationSchema = new Schema({
   causes: [
