5dd7b74ac7988c02625656150087d211ed4c895f
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-03-27 12:09:12 -0700

    added login valiation

--- client/src/components/forms/Signin.js
+++ client/src/components/forms/Signin.js
@@ -8,135 +8,193 @@ import { useUserContext } from "../../utils/GlobalStates/UserContext";
 import { GET_USER_INFO, USER_LOADING } from "../../utils/actions/actions";
 
 const useStyles = makeStyles({
-  paper: {
-    background:
-      "linear-gradient( 90deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 0% )",
-    borderRadius: "0px",
-    boxShadow: "0 3.42857px 23px rgb(0 0 0 / 10%)",
-    padding: "20px"
-  },
-  centerPosition: {
-    padding: "20px",
-    textAlign: "center"
-  },
-  centerContainer: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)"
-  },
-  bgstyle: {
-    color: "#3f4d67"
-  },
-  mgstyle: {
-    marginTop: "5px",
-    marginBottom: "5px"
-  },
-  styleMain: {
-    background: "linear-gradient(-135deg,#1de9b6,#1dc4e9)",
-    color: "#ffffff",
-    padding: "15px",
-    borderRadius: "0px"
-  },
-  styleSecondary: {
-    background: "linear-gradient(-135deg,#899fd4,#a389d4)",
-    color: "#ffffff"
-  },
-  styleIcon: {
-    background: "#3f4d67"
-  }
+	paper: {
+		background:
+			"linear-gradient( 90deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 0% )",
+		borderRadius: "0px",
+		boxShadow: "0 3.42857px 23px rgb(0 0 0 / 10%)",
+		padding: "20px",
+	},
+	centerPosition: {
+		padding: "20px",
+		textAlign: "center",
+	},
+	centerContainer: {
+		position: "absolute",
+		top: "50%",
+		left: "50%",
+		transform: "translate(-50%, -50%)",
+	},
+	bgstyle: {
+		color: "#3f4d67",
+	},
+	mgstyle: {
+		marginTop: "5px",
+		marginBottom: "5px",
+	},
+	styleMain: {
+		background: "linear-gradient(-135deg,#1de9b6,#1dc4e9)",
+		color: "#ffffff",
+		padding: "15px",
+		borderRadius: "0px",
+	},
+	styleSecondary: {
+		background: "linear-gradient(-135deg,#899fd4,#a389d4)",
+		color: "#ffffff",
+	},
+	styleIcon: {
+		background: "#3f4d67",
+	},
 });
 export default function Signin() {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  const [stateSignIn, setStateSignIn] = useState({
-    email: "",
-    username: ""
-  });
+	const [stateSignIn, setStateSignIn] = useState({
+		email: "",
+		emailError: "",
+		username: "",
+		usernameError: "",
+	});
 
-  const history = useHistory();
-  // Get the helper login function from the `useLogin` hook.
-  const login = useLogin();
+	const history = useHistory();
+	// Get the helper login function from the `useLogin` hook.
+	const login = useLogin();
 
-  const handleChange = function(event) {
-    const { name, value } = event.target;
-    setStateSignIn({
-      ...stateSignIn,
-      [name]: value
-    });
-  };
-  const [, userDispatch] = useUserContext();
-  const handleSubmit = async event => {
-    event.preventDefault();
+	const handleChange = function(event) {
+		const { name, value } = event.target;
+		setStateSignIn({
+			...stateSignIn,
+			[name]: value,
+		});
+	};
+	const [, userDispatch] = useUserContext();
+	const handleSubmit = async event => {
+		event.preventDefault();
 
-    try {
-      const { _id } = await login(stateSignIn);
+		try {
+			const { _id } = await login(stateSignIn);
 
-      // User has been successfully logged in and added to state. Perform any additional actions you need here such as redirecting to a new page.
+			// User has been successfully logged in and added to state. Perform any additional actions you need here such as redirecting to a new page.
 
-      await userDispatch({ type: USER_LOADING });
-      await userDispatch({
-        type: GET_USER_INFO,
-        payload: {
-          _id,
-          loading: false
-        }
-      });
+			await userDispatch({
+				type: USER_LOADING,
+			});
+			await userDispatch({
+				type: GET_USER_INFO,
+				payload: {
+					_id,
+					loading: false,
+				},
+			});
 
-      history.push("/newsfeed");
-    } catch (err) {
-      // Handle error responses from the API
-      if (err.response && err.response.data) console.log(err.response.data);
-    }
-  };
+			history.push("/newsfeed");
+		} catch (err) {
+			// Handle error responses from the API
+			if (err.response && err.response.data) {
+				console.log(err.response.data);
+				validateLogin(event, err.response.data);
+			}
+		}
+	};
+	// Form validation for inputs to be more than 6 characters
+	const validateLogin = (event, response) => {
+		const { name } = event.target;
+		console.log(name);
+		let isError = false;
+		const errors = {};
 
-  return (
-    <Grid
-      container
-      direction='column'
-      justify='center'
-      alignItems='center'
-      className={classes.paper}
-    >
-      <Grid item align='center'>
-        <Avatar className={classes.styleIcon}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography variation='h6' color='default'>
-          Log In
-        </Typography>
-      </Grid>
-      <form autoComplete='off' onSubmit={handleSubmit}>
-        <TextField
-          name='username'
-          value={stateSignIn.username}
-          onChange={handleChange}
-          variant='outlined'
-          label='Username'
-          placeholder='Enter Username'
-          fullWidth
-          className={classes.mgstyle}
-        />
-        <TextField
-          name='password'
-          value={stateSignIn.password}
-          onChange={handleChange}
-          variant='outlined'
-          label='Password'
-          placeholder='Enter Password'
-          type='password'
-          fullWidth
-          className={classes.mgstyle}
-        />
-        <Button
-          size='large'
-          className={classes.styleMain}
-          fullWidth
-          onClick={handleSubmit}
-        >
-          Log In
-        </Button>
-      </form>
-    </Grid>
-  );
+		if (!response.data.username) {
+			errors[`${name}Error`] = "Wrong LogIn Information";
+			setStateSignIn({
+				...stateSignIn,
+				...errors,
+			});
+		}
+
+		return isError;
+	};
+
+	// Form validation for inputs to be more than 6 characters
+	const validate = event => {
+		const { name, value } = event.target;
+		console.log(name);
+		let isError = false;
+		const errors = {};
+		if (value.length < 1) {
+			isError = true;
+			errors[`${name}Error`] = "Input cannot be empty";
+		}
+		console.log(value.length);
+		if (isError) {
+			setStateSignIn({
+				...stateSignIn,
+				...errors,
+			});
+		}
+		if (value.length >= 1) {
+			errors[`${name}Error`] = "";
+			setStateSignIn({
+				...stateSignIn,
+				...errors,
+			});
+		}
+
+		return isError;
+	};
+
+	return (
+		<Grid
+			container
+			direction='column'
+			justify='center'
+			alignItems='center'
+			className={classes.paper}
+		>
+			<Grid item align='center'>
+				<Avatar className={classes.styleIcon}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography variation='h6' color='default'>
+					Log In
+				</Typography>
+			</Grid>
+			<form autoComplete='off' onSubmit={handleSubmit}>
+				<TextField
+					error={stateSignIn.usernameError}
+					helperText={stateSignIn.usernameError}
+					name='username'
+					value={stateSignIn.username}
+					onChange={handleChange}
+					onBlur={validate}
+					variant='outlined'
+					label='Username'
+					placeholder='Enter Username'
+					fullWidth
+					className={classes.mgstyle}
+				/>
+				<TextField
+					error={stateSignIn.passwordError}
+					helperText={stateSignIn.passwordError}
+					name='password'
+					value={stateSignIn.password}
+					onChange={handleChange}
+					onBlur={validate}
+					variant='outlined'
+					label='Password'
+					placeholder='Enter Password'
+					type='password'
+					fullWidth
+					className={classes.mgstyle}
+				/>
+				<Button
+					size='large'
+					className={classes.styleMain}
+					fullWidth
+					onClick={handleSubmit}
+				>
+					Log In
+				</Button>
+			</form>
+		</Grid>
+	);
 }
