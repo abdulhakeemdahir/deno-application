439a18015b47b9449deb138e6ccb938eb6072eb8
Taani Maama <taani1maama@live.com>
2021-03-29 14:58:16 -0700

    Fixed footer styling.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,7 +1,36 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { Typography, Grid, ListItem } from "@material-ui/core";
+import { Typography, Grid, ListItem, makeStyles } from "@material-ui/core";
 import { LinkedIn } from "@material-ui/icons";
-import useFooterStyles from "./useFooterStyles";
+// import useFooterStyles from "./useFooterStyles";
+
+const useFooterStyles = makeStyles({
+  footerStyle: {
+    // bottom: "20px",
+    position: "relative",
+    // height: "45vh",
+    width: "100%",
+    zIndex: "1",
+    color: "#008394 !important",
+    // padding: "10px",
+    textAlign: "center",
+    justifyContent: "center"
+  },
+  heart: {
+    color: "#ed4b82"
+  },
+  bottomPadding: {
+    marginBottom: "20px"
+  },
+  linkedIn: {
+    position: "relative",
+    width: "100%",
+    zIndex: "1",
+    color: "#ed4b82 !important",
+    textAlign: "center",
+    justifyContent: "center",
+    flexFlow: "wrap"
+  }
+});
 
 const Footer = () => {
   const classes = useFooterStyles();
