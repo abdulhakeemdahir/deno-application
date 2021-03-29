87b04ecac125994b9f983e3e390e1d5efb4255a7
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-03-28 18:00:42 -0700

    added linking to public news component

--- client/src/components/News/index.js
+++ client/src/components/News/index.js
@@ -15,6 +15,8 @@ import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 import "./style.css";
+import { Link } from "react-router-dom";
+import { useUserContext } from "../../utils/GlobalStates/UserContext";
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -57,79 +59,91 @@ const useStyles = makeStyles(theme => ({
 
 export default function News(props) {
 	const classes = useStyles();
+
+	const [userState] = useUserContext();
+
 	return (
-    <>
-      <Grid item className="card" xs={12}>
-        <Grid container className="headerContainer">
-          <Grid item xs={9} sm={10}>
-            <Typography variant="subtitle1" style={{ fontWeight: "bold" }}>
-              {props.title}
-            </Typography>
-          </Grid>
-        </Grid>
-        <Typography variant="body2" color="textSecondary" component="p">
-          <span className="authorStyle"> Author:</span> {props.author}
-        </Typography>
-        <Divider />
-        <Grid container direction="row" spacing={1}>
-          <Grid item xs={12} sm={4}>
-            <CardMedia
-              className={"media"}
-              image={`https://res.cloudinary.com/astralgnome/image/upload/${props.image}`}
-            />
-          </Grid>
-          <Grid item xs={12} sm={8}>
-            <CardContent>
-              <Typography variant="body" color="textSecondary" component="p">
-                {props.post}
-              </Typography>
-              <a href={props.link} className="hashTagStyle">
-                #{props.hashTag}
-              </a>
-            </CardContent>
-            <Divider />
-          </Grid>
-        </Grid>
-        <Grid container xs={12} spacing={1}>
-          <Accordion className={classes.shadow}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon className={classes.commentStyle} />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography className={classes.heading}>
-                Read {props.comments.length} Comments
-              </Typography>
-            </AccordionSummary>
-            <Grid className="cardComment">
-              {props.comments.map((card) => (
-                <AccordionDetails>
-                  <Grid container xs={12} className={classes.gridStyle}>
-                    <Grid item xs={4}>
-                      <Typography
-                        variant="body"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {card.user.firstName}
-                      </Typography>
-                    </Grid>
-                    <Grid item xs={8}>
-                      <Typography
-                        variant="body"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {card.content}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </AccordionDetails>
-              ))}
-            </Grid>
-          </Accordion>
-        </Grid>
-      </Grid>
-    </>
-  );
+		<>
+			<Grid item className='card' xs={12}>
+				<Grid container className='headerContainer'>
+					<Grid item xs={9} sm={10}>
+						<Typography variant='subtitle1' style={{ fontWeight: "bold" }}>
+							{props.title}
+						</Typography>
+					</Grid>
+				</Grid>
+				<Typography variant='body2' color='textSecondary' component='p'>
+					<span className='authorStyle'> Author:</span>
+					<Link
+						to={
+							props.authorId === userState._id
+								? "/dashboard/:id"
+								: `/dashboard/:id${props.authorId}`
+						}
+					>
+						{props.author}
+					</Link>
+				</Typography>
+				<Divider />
+				<Grid container direction='row' spacing={1}>
+					<Grid item xs={12} sm={4}>
+						<CardMedia
+							className={"media"}
+							image={`https://res.cloudinary.com/astralgnome/image/upload/${props.image}`}
+						/>
+					</Grid>
+					<Grid item xs={12} sm={8}>
+						<CardContent>
+							<Typography variant='body' color='textSecondary' component='p'>
+								{props.post}
+							</Typography>
+							<a href={props.link} className='hashTagStyle'>
+								#{props.hashTag}
+							</a>
+						</CardContent>
+						<Divider />
+					</Grid>
+				</Grid>
+				<Grid container xs={12} spacing={1}>
+					<Accordion className={classes.shadow}>
+						<AccordionSummary
+							expandIcon={<ExpandMoreIcon className={classes.commentStyle} />}
+							aria-controls='panel1a-content'
+							id='panel1a-header'
+						>
+							<Typography className={classes.heading}>
+								Read {props.comments.length} Comments
+							</Typography>
+						</AccordionSummary>
+						<Grid className='cardComment'>
+							{props.comments.map(card => (
+								<AccordionDetails>
+									<Grid container xs={12} className={classes.gridStyle}>
+										<Grid item xs={4}>
+											<Typography
+												variant='body'
+												color='textSecondary'
+												component='p'
+											>
+												{card.user.firstName}
+											</Typography>
+										</Grid>
+										<Grid item xs={8}>
+											<Typography
+												variant='body'
+												color='textSecondary'
+												component='p'
+											>
+												{card.content}
+											</Typography>
+										</Grid>
+									</Grid>
+								</AccordionDetails>
+							))}
+						</Grid>
+					</Accordion>
+				</Grid>
+			</Grid>
+		</>
+	);
 }
