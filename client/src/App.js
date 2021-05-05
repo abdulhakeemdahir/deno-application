8126b7973136b36a101e782fdd1b8a594989c9cb
Taani Maama <taani1maama@live.com>
2021-05-04 20:41:21 -0700

    Created the Providers Component so that the App file will be a bit smaller.

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,15 @@ import Landing from "./containers/Public/Landing";
 import Explore from "./containers/Public/Explore";
 import Newsfeed from "./containers/Private/Newsfeed";
 import Dashboard from "./containers/Private/Dashboard";
-import { UserProvider } from "./utils/GlobalStates/UserContext";
-import { GlobalProvider } from "./utils/GlobalStates/GlobalState";
-import { CauseProvider } from "./utils/GlobalStates/CauseContext";
-import { PostProvider } from "./utils/GlobalStates/PostContext";
 import { useAuthTokenStore } from "./utils/auth.js";
 import { useSocketConnection } from "./utils/GlobalStates/SocketProvider";
 import PrivateRoute from "./components/PrivateRoute.js";
+import Providers from "./components/Providers.js";
 import Chatroom from "./containers/Private/Chatroom";
 import Search from "./containers/Private/Search";
 import SinglePost from "./containers/SinglePost";
 import Analytics from "./containers/Private/Analytics";
 import ErrorPage from "./containers/Public/ErrorPage";
-import { TrendingProvider } from "./utils/GlobalStates/TrendingContext";
-import { ConvoProvider } from "./utils/GlobalStates/ConvoContext";
-import { GuessProvider } from "./utils/GlobalStates/GuessContext";
 import PublicDash from "./containers/Public/PublicDash";
 // import { Settings } from "@material-ui/icons";
 import SettingsPage from "./containers/Private/Settings";
@@ -46,74 +40,51 @@ function App() {
     <Router>
       <MuiThemeProvider theme={theme}>
         <div className="App">
-          <GlobalProvider>
-            <UserProvider>
-              <GuessProvider>
-                <CauseProvider>
-                  <TrendingProvider>
-                    <PostProvider>
-                      <ConvoProvider>
-                        <Switch>
-                          <PrivateRoute
-                            exact
-                            path="/newsfeed"
-                            redirectTo="/"
-                            component={Newsfeed}
-                          />
-
-                          <PrivateRoute
-                            exact
-                            path="/dashboard"
-                            redirectTo="/"
-                            component={Dashboard}
-                          />
-                          <PrivateRoute
-                            exact
-                            path="/chatroom"
-                            redirectTo="/"
-                            component={Chatroom}
-                          />
-                          <PrivateRoute
-                            exact
-                            path="/dashboard/:id"
-                            redirectTo="/"
-                            component={PublicDash}
-                          />
-                          <PrivateRoute
-                            exact
-                            path="/search/:action/:search"
-                            redirectTo="/"
-                            component={Search}
-                          />
-                          <PrivateRoute
-                            exact
-                            path="/settings"
-                            redirectTo="/"
-                            component={ SettingsPage }
-                          />
-
-                          <Route path="/explore" exact component={Explore} />
-                          <Route
-                            path="/post/:id"
-                            exact
-                            component={SinglePost}
-                          />
-                          <Route
-                            path="/analytics"
-                            exact
-                            component={Analytics}
-                          />
-                          <Route path="/404" exact component={ErrorPage} />
-
-                          <Route path="/" exact component={Landing} />
-                        </Switch>
-                      </ConvoProvider>
-                    </PostProvider>
-                  </TrendingProvider>
-                </CauseProvider>
-              </GuessProvider>
-            </UserProvider>
-          </GlobalProvider>
+          <Providers>
+            <Switch>
+              <PrivateRoute
+                exact
+                path="/newsfeed"
+                redirectTo="/"
+                component={Newsfeed}
+              />
+              <PrivateRoute
+                exact
+                path="/dashboard"
+                redirectTo="/"
+                component={Dashboard}
+              />
+              <PrivateRoute
+                exact
+                path="/chatroom"
+                redirectTo="/"
+                component={Chatroom}
+              />
+              <PrivateRoute
+                exact
+                path="/dashboard/:id"
+                redirectTo="/"
+                component={PublicDash}
+              />
+              <PrivateRoute
+                exact
+                path="/search/:action/:search"
+                redirectTo="/"
+                component={Search}
+              />
+              <PrivateRoute
+                exact
+                path="/settings"
+                redirectTo="/"
+                component={SettingsPage}
+              />
+              <Route path="/explore" exact component={Explore} />
+              <Route path="/post/:id" exact component={SinglePost} />
+              <Route path="/analytics" exact component={Analytics} />
+              <Route path="/404" exact component={ErrorPage} />
+              <Route path="/" exact component={Landing} />
+            </Switch>
+          </Providers>
         </div>
       </MuiThemeProvider>
     </Router>
