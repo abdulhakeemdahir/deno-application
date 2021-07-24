a1e514de04bdab17097d27efcb2bc5c5c707e023
Taani Maama <taani1maama@live.com>
2021-07-24 11:51:07 -0700

    Added test for invalid firstName and lastname inputs for users.

--- server/test/user/test_user_model.js
+++ server/test/user/test_user_model.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 process.env.NODE_ENV = "test";
 
-const assert = require("assert");
-
 // const app = require("../server");
 
 const { User } = require("../../models");
@@ -11,6 +9,7 @@ const mongoose = require("mongoose");
 
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const { assert, expect } = require("chai");
 
 chai.use(chaiHttp);
 
@@ -23,133 +22,145 @@ const validUserObject = {
   role: "Personal"
 };
 
-// Model Testing
-describe("User model creation testing", () => {
+describe("User Model Testing", () => {
   before(async () => {
-    const dbTestUrl = process.env.TEST_DB;
-    mongoose
-      .connect(dbTestUrl, {
+    try {
+      const dbTestUrl = process.env.TEST_DB;
+      await mongoose.connect(dbTestUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
-      })
-      .then(() => console.log("Database connected!"))
-      .catch(err => console.log(err));
-    await User.deleteMany();
-  });
-
-  // Clears the db so we can keep testing.
-  afterEach(async () => {
-    await User.deleteMany();
-  });
-
-  it("Test user creation with valid email and password", async () => {
-    try {
-      const user = await User.create(validUserObject).catch(err => {
-        console.log(err);
       });
-
-      await user.save();
-      assert(!user.isNew);
     } catch (error) {
-      console.log(error);
+      throw new Error(err.message);
     }
   });
 
-  it("Test user creation with invalid email", async () => {
-    let e = null;
+  // Clears the db so we can keep testing.
+  after(async () => {
+    await User.deleteMany();
+  });
 
-    try {
-      const user = await User.create({
-        firstName: "Mocha",
+  describe("User Create Testing", () => {
+    before(async () => {
+      await User.deleteMany();
+    });
+
+    // Clears the db so we can keep testing.
+    afterEach(async () => {
+      await User.deleteMany();
+    });
+
+    it("Test user creation with valid email and password", async () => {
+      try {
+        const user = await User.create(validUserObject).catch(err => {
+          console.log(err);
+        });
+
+        await user.save();
+        assert(!user.isNew);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    it("Test failed user creation with invalid email", async () => {
+      let e = null;
+
+      try {
+        const user = await User.create({
+          firstName: "Mocha",
+          lastname: "test",
+          username: "mochatest",
+          email: "testing@.com",
+          password: "testTest123$",
+          role: "Personal"
+        });
+
+        await user.validate();
+      } catch (error) {
+        e = error;
+      }
+
+      chai.assert.isNotNull(e, "Error should not be null");
+    });
+
+    it("Test failed user creation with invalid password", async () => {
+      let e = null;
+
+      try {
+        const user = await User.create({
+          firstName: "Mocha",
+          lastname: "test",
+          username: "mochatest",
+          email: "testing@test.com$",
+          password: "asdf",
+          role: "Personal"
+        });
+
+        await user.validate();
+      } catch (error) {
+        e = error;
+      }
+
+      chai.assert.isNotNull(e, "Error should not be null");
+    });
+
+    it("Test failed user creation with no first name", () => {
+      const user = new User({
+        firstName: "",
         lastname: "test",
         username: "mochatest",
-        email: "testing@.com",
+        email: "testing@test.com$",
         password: "testTest123$",
         role: "Personal"
       });
 
-      await user.validate();
-    } catch (error) {
-      e = error;
-    }
+      user.validate(err => {
+        expect(err).to.not.be.null;
+      });
+    });
 
-    chai.assert.isNotNull(e, "Error should not be null");
+    it("Test failed user creation with no last name", () => {
+      const user = new User({
+        firstName: "Mocha",
+        lastname: "",
+        username: "mochatest",
+        email: "testing@test.com$",
+        password: "testTest123$",
+        role: "Personal"
+      });
+
+      user.validate(err => {
+        expect(err).to.not.be.null;
+      });
+    });
   });
 
-  it("Test user creation with invalid password", async () => {
-    let e = null;
+  describe("User Read Testing", () => {
+    before(async () => {
+      await User.deleteMany();
 
-    try {
       const user = await User.create({
         firstName: "Mocha",
         lastname: "test",
         username: "mochatest",
-        email: "testing@test.com$",
-        password: "asdf",
-        role: "Personal"
-      });
-
-      await user.validate();
-    } catch (error) {
-      e = error;
-    }
-
-    chai.assert.isNotNull(e, "Error should not be null");
-  });
-
-  it("Should be able to find user in db after they've been created.", async () => {
-    try {
-      const user = await User.create(validUserObject).catch(err => {
-        console.log(err);
-      });
-
-      const secondUser = await User.create({
-        firstName: "Mocha",
-        lastname: "test",
-        username: "mochatest2",
-        email: "testing2@test.com",
+        email: "testing@test.com",
         password: "testTest123$",
         role: "Personal"
       }).catch(err => console.log(err));
 
-      const findUser = await User.findOne({ _id: user._id });
+      await user.save();
+    });
 
-      assert.deepStrictEqual(user._id, findUser._id);
-      assert.notDeepStrictEqual(secondUser._id, findUser._id);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-});
+    it("Test finding user in db after they've been created.", async () => {
+      try {
+        const findUser = await User.findOne({ username: "mochatest" });
 
-describe("User model read, update, and delete testing", () => {
-  before(async () => {
-    const dbTestUrl = process.env.TEST_DB;
-    mongoose
-      .connect(dbTestUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-      })
-      .then(() => console.log("Database connected!"))
-      .catch(err => console.log(err));
-    await User.deleteMany();
-
-    const user = await User.create({
-      firstName: "Mocha",
-      lastname: "test",
-      username: "mochatest",
-      email: "testing@test.com",
-      password: "testTest123$",
-      role: "Personal"
-    }).catch(err => console.log(err));
-
-    await user.save();
-  });
-
-  // Clears the db so we can keep testing.
-  afterEach(async () => {
-    await User.deleteMany();
+        chai.assert.isObject(findUser);
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    });
   });
 });
